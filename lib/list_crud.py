fb3b2a84126b52d992405e5aff8e45f7a16902e0
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-10-05 18:48:29 +0300

    List CRUD Lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,31 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return [1, 2, 3, 4]  # You can choose any four elements here.
 
-def add_element_to_end_of_list(l, element):
-    return None
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    return my_list
 
-def add_element_to_start_of_list(l, element):
-    return None
+def add_element_to_start_of_list(my_list, element):
+    my_list.insert(0, element)
+    return my_list
 
-def remove_element_from_end_of_list(l):
-    return None
+def remove_element_from_end_of_list(my_list):
+    my_list.pop()
+    return my_list
 
-def remove_element_from_start_of_list(l):
-    return None
+def remove_element_from_start_of_list(my_list):
+    del my_list[0]
+    return my_list
 
-def retrieve_first_element_from_list(l):
-    return None
+def retrieve_first_element_from_list(my_list):
+    return my_list[0]
 
-def retrieve_element_from_index(l, index):
-    return None
+def retrieve_element_from_index(my_list, index):
+    return my_list[index]
 
-def retrieve_last_element_from_list(l):
-    return None
+def retrieve_last_element_from_list(my_list):
+    return my_list[-1]
+
